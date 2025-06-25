525642b2d9d3324da6218ea64211b52a12750d61
GrungeElFz <gdelviora@0x01000111.io>
2025-06-24 21:06:09 -0500

    Modified:   Refined `<SheetPrimitive.Close />` position, size, and hover effect (src/components/ui/sheet.tsx)

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -70,8 +70,13 @@ function SheetContent({
         {...props}
       >
         {children}
-        <SheetPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute top-8 right-10 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none">
-          <XIcon className="size-5" />
+        <SheetPrimitive.Close
+          className={cn(
+            "absolute top-7 right-9 w-10 h-10 flex items-center justify-center rounded-md opacity-100",
+            className   
+          )}
+        >
+          <XIcon className="size-5 cursor-pointer hover:text-white" />
           <span className="sr-only">Close</span>
         </SheetPrimitive.Close>
       </SheetPrimitive.Content>
