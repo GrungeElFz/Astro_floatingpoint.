baca9515c1e0c5b1ef86e1cb2adc08fc81b06d8c
GrungeElFz <gdelviora@0x01000111.io>
2025-07-03 00:59:52 -0500

    Modified:   Updated EventSection header section for consistency across other header sections (src/components/events/EventSection.tsx) Modified:   Updated SetSection header section for consistency across other header sections (src/components/sets/SetSection.tsx) Modified:   Updated VideoSection header section for consistency across other header sections (src/components/videos/VideoSection.tsx)

--- src/components/sets/SetSection.tsx
+++ src/components/sets/SetSection.tsx
@@ -165,19 +165,19 @@ export const SetSection: React.FC<SetsSectionProps> = ({
   };
 
   return (
-    <section className="bg-black text-neutral-100 py-16 pb-24">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8 text-center">
+    <section className="bg-black text-neutral-100 py-16 sm:py-24">
+      <div className="mx-auto max-w-7xl py-4 px-6 lg:px-8 text-center">
         <div className="mb-12">
           {displayMode === "grid" ? (
             <h1 className="text-5xl sm:text-6xl font-bold mb-4">
               Recorded Sets
             </h1>
           ) : (
-            <h2 className="text-4xl sm:text-5xl font-bold mb-4">
+            <h1 className="text-5xl sm:text-6xl font-bold mb-4">
               <a href="/sets">Recorded Sets</a>
-            </h2>
+            </h1>
           )}
-          <p className="text-lg text-neutral-400 max-w-2xl mx-auto">
+          <p className="text-lg font-normal italic text-pretty text-neutral-400 sm:text-xl/8 mx-auto mb-12 mt-8">
             Dive into our archive of live performances and exclusive mixes from
             underground electronic artists.
           </p>
