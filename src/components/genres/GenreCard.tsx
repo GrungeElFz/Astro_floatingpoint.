c629eacdf223afa3ef65a230ceb515c2982bf45c
GrungeElFz <gdelviora@0x01000111.io>
2025-07-18 22:12:40 -0500

    Modified:   Removed toggle feature and display Spotify embed by default (src/components/genres/GenreCard.tsx)

--- src/components/genres/GenreCard.tsx
+++ src/components/genres/GenreCard.tsx
@@ -1,7 +1,5 @@
 import * as React from "react";
-import { useState } from "react";
 import type { GenreWithSpotifyData } from "@/types/genres";
-import { Play } from "lucide-react";
 
 interface GenreCardProps {
   genre: GenreWithSpotifyData;
@@ -14,37 +12,22 @@ export const GenreCard: React.FC<GenreCardProps> = ({
   className,
   onCardClick,
 }) => {
-  const [isPlayerVisible, setIsPlayerVisible] = useState(false);
-
-  const handleCardClick = () => {
-    setIsPlayerVisible(!isPlayerVisible);
-    if (onCardClick) {
-      onCardClick();
-    }
-  };
-
   return (
     <div
-      onClick={handleCardClick}
-      className={`group block rounded-3xl overflow-hidden backdrop-blur-sm bg-white/5 border border-white/10 text-neutral-300 hover:bg-white/10 hover:border-white/20 transition-all duration-300 ease-in-out cursor-pointer ${className}`}
+      onClick={onCardClick}
+      className={`group rounded-3xl overflow-hidden backdrop-blur-sm bg-white/5 border border-white/10 text-neutral-300 h-full flex flex-col ${className}`}
     >
-      <div className="p-6">
-        <h3 className="text-lg font-bold text-white mb-2 transition-colors group-hover:text-cyan-300">
+      <div className="p-6 flex flex-col flex-grow">
+        <h3 className="text-lg font-bold text-white mb-2 group-hover:text-cyan-300">
           {genre.name}
         </h3>
-        <p className="text-sm font-light text-neutral-400 line-clamp-3 mb-8">
+        <p className="text-sm font-light text-neutral-400 mb-4">
           {genre.description}
         </p>
 
-        <div className="h-5">
+        <div className="h-5 mt-auto pt-4">
           {genre.artist && genre.trackName && (
             <div className="flex items-center text-gray-400">
-              <Play
-                size={16}
-                className={`mr-2 flex-shrink-0 transition-transform duration-300 ${
-                  isPlayerVisible ? "rotate-90" : ""
-                }`}
-              />
               <p className="text-sm truncate">
                 {genre.artist} - {genre.trackName}
               </p>
@@ -53,7 +36,7 @@ export const GenreCard: React.FC<GenreCardProps> = ({
         </div>
       </div>
 
-      {isPlayerVisible && genre.spotifyTrackId && (
+      {genre.spotifyTrackId && (
         <div className="p-2 pt-0">
           <iframe
             style={{ borderRadius: "12px" }}
