697e6224207f8618375b2af7d74e7884b136ff9d
GrungeElFz <gdelviora@0x01000111.io>
2025-07-29 13:13:02 -0500

    Modified:   Added `<HostedVideoSection />` into `<VideoSection />` component (src/components/videos/VideoSection.tsx)

--- src/components/videos/VideoSection.tsx
+++ src/components/videos/VideoSection.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useVideos } from "@/hooks/useVideos";
 import { YouTubeFeature } from "@/components/videos/youtube/YouTubeFeature";
 import { YouTubeSection } from "@/components/videos/youtube/YouTubeSection";
+import { HostedVideoSection } from "@/components/videos/hosted/HostedVideoSection";
 import { Skeleton } from "@/components/ui/skeleton";
 
 export const VideoSection: React.FC = () => {
@@ -32,6 +33,7 @@ export const VideoSection: React.FC = () => {
         </div>
 
         <YouTubeSection videos={gridVideos} isLoading={loading} />
+        <HostedVideoSection />
       </div>
     </div>
   );
